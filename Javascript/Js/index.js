4eeb5a8971f4db770378bc4cbb92cf5cb33498d4
Your Name <mandlasrinivasulu9685@gmail.com>
2023-08-25 14:39:13 +0530

    added

---
+++ Javascript/Js/index.js
@@ -0,0 +1,106 @@
+console.log("Srinivas");
+
+//Fundamental Statements
+//Varaibles
+var a='malli';
+console.log(a);
+
+//String
+var name="sunnel";
+console.log(name);
+
+//Boolean
+var b=false;
+console.log(b);
+
+//undefine
+var c;
+console.log(c);
+
+//Number
+var d=20;
+console.log(d);
+
+//Type Coercion
+var e='seetha';
+console.log(e);
+
+console.log(a+' is a good boy and bad boy is a '+name);
+
+var schoolname,hascourse
+
+schoolname="udemy academy";
+hascourse=true;
+console.log(schoolname+' is Good learning Platform for the beginner? '+hascourse);
+
+var schoolteam='Volleyball';
+console.log(schoolteam);
+//variable mutation
+d='twenty';
+console.log(d);
+
+schoolteam='Kabbadi team';
+console.log(schoolteam);
+
+//Alert
+
+// alert(schoolteam);
+
+//Promt
+// var age=prompt('How Old is He');
+// console.log('Chandu is '+age+' years old.');
+
+//Basic Operators
+//Addition Operators
+let x=8;
+let y=2;
+console.log(x+y);
+//Subtraction 
+let p=9;
+let q=5;
+console.log(p-q);
+//Multiplication
+let r=10;
+let s=10;
+console.log(r*s);
+//Division
+let t=10;
+let u=2;
+console.log(t/u);
+//modulus
+let v=10;
+let w=2;
+console.log(v%w);
+
+//Conditional Statements
+var grade=32;
+if (grade<30) {
+    console.log('you can not go to the next class');
+}
+else if(grade>=30 || grade<30){
+    console.log('you can not go to the next class but you enter the exam');
+}
+else{
+    console.log('you can go to the next class');
+}
+
+//Quiz
+var age;
+age=16;
+if (age<15) {
+    console.log('You can not enter the cafe');
+}
+else if(age<23&&age<=15){
+    console.log('you can enter the cafe but can not buy a drink');
+}
+else{
+    console.log('you can enter the cafe and buy a drink')
+}
+
+//Ternary Operator
+//syntax:condition?('statement1'):('statement2');
+
+var marks;
+marks=60;
+marks<70?console.log('failure'):console.log('suuccess');
+
---
+++ Javascript/Js/skill.js
@@ -0,0 +1,50 @@
+var name='john';
+console.log(name);
+
+//Data Types
+//number,string,boolean,undefine
+
+var a=2;
+console.log(typeof a);
+var a="gihrwF#O5THIGJOVKC"
+console.log(typeof a)
+ 
+var a=true;
+console.log(typeof a);
+
+var b;
+console.log(typeof b);
+
+//Arrays
+var data=[1,2,3,4,5,6];
+//console.log(data);
+console.log(data[4]);
+
+//Objects
+var data={
+    name:'suneel',
+    age:30
+}
+console.log(data.name);
+
+//Functions
+function user(){
+    console.log('hello world');
+}
+user();
+// function user(a){
+//     console.log(a);
+// }
+// user(20);
+//Conditional Statements
+var marks=50;
+if(marks>78){
+    console.log('Grade A')
+}
+else if(marks>60){
+    console.log('Grade B');
+}
+else{
+    console.log('Grade C')
+}
+
